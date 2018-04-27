efe7df5d83790402e64ee6108efce004f9d9ddb5
Limiteddd <38568217+Limiteddd@users.noreply.github.com>
2018-04-27 22:56:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,6 +18,36 @@ client.on('message', message => {
     if (message.content === 'bazdmeg roky') {
     	message.reply('háháháháhá autista :smile:');
   	}
+    if (message.content === 'pofon') {
+    	message.reply('Engem nem is pofozott fel. :open_mouth:');
+  	}
+     if (message.content === 'lagg') {
+    	message.reply('vájá belaggoltam');
+  	}
+     if (message.content === 'mérges') {
+    	message.reply('Hát szerintem csak te vagy az,te autista balfasz.');
+  	}
+    if (message.content === 'mérges?') {
+    	message.reply('Hát szerintem csak te vagy az,te autista balfasz.');
+  	}
+    if (message.content === 'autista') {
+    	message.reply('CryFace.');
+  	}
+     if (message.content === 'miért haltál meg?') {
+    	message.reply('Editbug.');
+  	}
+    if (message.content === 'roky') {
+    	message.reply('Forrás:Wikipédia. Roky egy koraszülött agybajos autista csoport. Általában 10 évesen elhunynak de a full kretének életbe maradnak 15 évig is.');
+  	}
+     if (message.content === 'amúgy ádám') {
+    	message.reply(':kussolj már cryface senkit nem érdekelsz');
+  	}
+      if (message.content === 'dagadt') {
+    	message.reply('geci');
+  	}
+     if (message.content === 'macska') {
+    	message.reply('fasz');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
